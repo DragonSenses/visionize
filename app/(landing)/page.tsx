34a9d048141f8f8a86cc10edf27fe1548d0210af
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-09 21:05:08 -0800

    Update landing page with badge and icons

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Medal } from 'lucide-react';
+import { ClipboardCheck, KanbanSquare } from 'lucide-react';
 
 export default function LandingPage() {
   return (
     <div className='flex items-center justify-center flex-col'>
       <div className='flex items-center justify-center flex-col'>
-        <div>
-          <Medal className='h-6 w-6 mr-2'/>
-          Achieve more with <strong><em>Visionize</em></strong>, the ultimate task management app
+        <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
+          <KanbanSquare className='h-6 w-6 mr-2'/>
+          <span className='mr-1'><strong>Visionize</strong></span> your tasks
+          <ClipboardCheck className='h-6 w-6 mx-2'/>
         </div>
       </div>
     </div>
