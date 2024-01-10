e55fe8fbf25751e2c9a6221a23c2c01b59a199d0
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-09 20:31:57 -0800

    Add styles and icon to landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Medal } from 'lucide-react';
 
 export default function LandingPage() {
   return (
-    <div>page</div>
+    <div className='flex items-center justify-center flex-col'>
+      <div className='flex items-center justify-center flex-col'>
+        <div>
+          <Medal className='h-6 w-6 mr-2'/>
+          Achieve more with <strong><em>Visionize</em></strong>, the ultimate task management app
+        </div>
+      </div>
+    </div>
   )
 }
