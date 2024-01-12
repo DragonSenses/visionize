b9c38719bf4fc26baebbc0de1db4034e469e3c39
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-11 16:09:31 -0800

    Add heading and text fonts to landing page
    - Use localFont and Poppins from next/font
    - Add KanbanSquare and ClipboardCheck icons from lucide-react
    - Add padding to text between icons
    - Add gradient background and padding to title and subtitle

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import Link from 'next/link';
 import localFont from 'next/font/local';
+import { Poppins } from 'next/font/google';
 import { ClipboardCheck, KanbanSquare } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+
 const headingFont = localFont({
   src: "../../public/fonts/CalSans-SemiBold.woff2",
 });
 
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+});
+
+
 export default function LandingPage() {
   return (
     <div className='flex items-center justify-center flex-col'>
-      <div className='flex items-center justify-center flex-col'>
+      <div className={cn(
+        'flex items-center justify-center flex-col',
+        headingFont.className,
+        )}>
+        {/* Icon Container */}
         <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
           <KanbanSquare className='h-6 w-6 mr-2'/>
-          Kanban your way
+          <span className='pt-1'>Kanban your way</span>
           <ClipboardCheck className='h-6 w-6 mx-2'/>
         </div>
+        {/* Headings */}
         <h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>
           <span className='mr-1'><strong>Visionize</strong></span> your tasks
         </h1>
@@ -25,13 +48,18 @@ export default function LandingPage() {
           Turn your vision into reality.
         </div>
       </div>
-      <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
+      {/* Promotional Text */}
+      <div className={cn(
+        'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+        textFont.className,
+      )}>
         Fight mediocrity with Visionize. With just boards, lists and cards you can 
         get a clear overview of your tasks. Then you can plan, prioritize, and
         execute your goals with confidence.      
         You can drag and drop tasks, add labels and due dates, attach files 
         and comments, and more.
       </div>
+      {/* Sign-Up Link */}
       <Button className='mt-6' size='lg' asChild>
         <Link href="/sign-up">
           Try Visionize for free
