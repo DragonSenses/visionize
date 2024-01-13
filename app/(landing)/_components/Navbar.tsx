8c51f20f5876fa0ff6c759dc90ac20a9de85bd73
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-12 17:17:58 -0800

    Update Navbar component with logo
    Adds a new navbar component that renders a logo and a placeholder for a button. The navbar is fixed at the top of the page and has a border and a shadow. It uses the Logo component from '@/components/Logo' and tailwindcss for styling.

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Logo from '@/components/Logo';
+
 export const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
+      <div className='md:max-w-screen-2-xl mx-auto flex items-center w-full justify-between'>
+        <Logo />
+        {/* Button */}
+      </div>
     </div>
   );
 };
