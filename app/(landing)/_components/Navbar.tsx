629bb4be0685b7c45e6f3c4f402ce5a4b9214561
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-12 17:38:15 -0800

    Add sign-in and sign-up buttons for Navbar
    This commit adds two buttons for sign-in and sign-up to the Navbar component. The buttons use the Link component from 'next/link' to navigate to the corresponding pages.

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import Link from 'next/link';
 
 import Logo from '@/components/Logo';
+import { Button } from '@/components/ui/button';
 
 export const Navbar = () => {
   return (
     <div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
       <div className='md:max-w-screen-2-xl mx-auto flex items-center w-full justify-between'>
         <Logo />
-        {/* Button */}
+        <div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
+          <Button size='sm' asChild>
+            <Link href="/sign-up">
+              Sign Up
+            </Link>
+          </Button>
+          <Button size='sm' variant='outline' asChild>
+            <Link href="/sign-in">
+              Login
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
