75ceac9dea151f336d779daaa9ba9c0563f256a3
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-13 19:57:45 -0800

    Add footer component with links
    Create a responsive footer component that displays two buttons for privacy policy and terms of service.

--- app/(landing)/_components/Footer.tsx
+++ app/(landing)/_components/Footer.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Logo from '@/components/Logo';
+import { Button } from '@/components/ui/button';
+
 export const Footer = () => {
   return (
-<div>
-
-</div>
+    <div className='fixed bottom-0 w-full px-4 border-t flex items-center bg-slate-100'>
+      <div className='md:max-w-screen-2-xl mx-auto flex items-center w-full justify-between'>
+        <Logo />
+        <div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
+          <Button size='sm' variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size='sm' variant="ghost">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
