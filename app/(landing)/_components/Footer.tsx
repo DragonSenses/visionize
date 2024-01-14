45d8739c6c1b730bb4666b85e6572fa367f8b13a
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-13 20:15:12 -0800

    Refactor Footer with buttonVariants & Link
    Replace the Button component with buttonVariants and use the Link component from next.js to render the links. Resize links to improve user experience. Also center the content horizontally and vertically with flexbox.

--- app/(landing)/_components/Footer.tsx
+++ app/(landing)/_components/Footer.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
 
-import Logo from '@/components/Logo';
-import { Button } from '@/components/ui/button';
+import { buttonVariants } from "@/components/ui/button";
+
 
 export const Footer = () => {
   return (
-    <div className='fixed bottom-0 w-full px-4 border-t flex items-center bg-slate-100'>
-      <div className='md:max-w-screen-2-xl mx-auto flex items-center w-full justify-between'>
-        <Logo />
-        <div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
-          <Button size='sm' variant="ghost">
+    <div className='fixed bottom-0 w-full p-4 border-t flex items-center bg-slate-100'>
+      <div className='md:max-w-screen-2-xl mx-auto flex items-center w-full justify-center'>
+        <div className='space-x-4 md:block md:w-auto flex items-center justify-center w-full'>
+          <Link className={buttonVariants({ size: "lg", variant: "ghost" })} href="/privacy-policy">
             Privacy Policy
-          </Button>
-          <Button size='sm' variant="ghost">
+          </Link>
+          <Link className={buttonVariants({ size: "lg", variant: "ghost" })} href="/terms-of-service">
             Terms of Service
-          </Button>
+          </Link>
         </div>
       </div>
     </div>
