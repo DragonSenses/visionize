2c890e647c3671c17d41e6d46a761d67ef7cdcbb
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-13 21:06:18 -0800

    Add text to logo component & use custom font
    - Add the text next to the logo image and style it with Tailwind CSS classes.
    - Import the CalSans-SemiBold font from the local folder and use it for the text.
    - Use the cn function from the utils library to combine the classes.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import localFont from 'next/font/local';
 import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const headingFont = localFont({
+  src: "../public/fonts/CalSans-SemiBold.woff2",
+});
 
 const Logo = () => {
   return (
@@ -12,6 +19,12 @@ const Logo = () => {
           height={30}
           width={30}
         />
+        <p className={cn(
+          'text-xl text-neutral-700 px-1 pt-1',
+          headingFont.className,
+        )}>
+          Visionize
+        </p>
       </div>
     </Link>
   )
