d2ab9089079202efd28e5ca215f101fcc623e1ea
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-14 21:14:58 -0800

    Create metadata configuration to improve SEO
    Separate configuration data from the presentation logic. Will be used to customize metadata of various web pages.

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "Visionize",
+  description: `Visionize is a kanban-style productivity app that helps you
+  turn your vision into reality. Plan, prioritize, and execute your goals
+  with boards, lists, and cards. Visionize your tasks with visionary kanban
+  boards. Try Visionize for free today.`,
+};
