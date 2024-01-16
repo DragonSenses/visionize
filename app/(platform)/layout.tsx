c42c6a9cd3e3bdd8ab36dab102a30574d9a1acbc
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-15 18:16:56 -0800

    Create layout.tsx

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlatformLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div>PlatformLayout</div>
+  )
+}
+
+export default PlatformLayout
