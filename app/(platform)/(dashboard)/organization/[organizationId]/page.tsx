798751ed5b7eee7a8f41be2f1125cd5e778678cf
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-17 04:31:45 -0800

    Display organization ID in dashboard page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,13 @@
+import { auth } from '@clerk/nextjs';
 import React from 'react';
 
 const OrganizationIdPage = () => {
+  const { userId, orgId } = auth();
+
   return (
-    <div>Organization Page</div>
+    <div>
+      {`Organization: ${orgId}`}
+    </div>
   );
 };
 
