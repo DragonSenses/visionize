415af48120c500f824a806110d5421ccb144a09a
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-17 05:43:08 -0800

    Add create button & responsive styles to navbar
    This commit adds a new feature to the navbar that allows users to create new organizations. It also improves the appearance and functionality of the navbar on different screen sizes and devices.

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Plus } from 'lucide-react';
+
+import Logo from '@/components/Logo';
+import { Button } from '@/components/ui/button';
 
 export const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='flex items-center fixed px-4 z-10 top-0 w-full h-14 border-b shadow-sm bg-white'>
+      {/* Responsive Container */}
+      <div className='flex items-center gap-x-4'>
+        {/* For screens 768px and larger  */}
+        <div className='hidden md:flex'>
+          <Logo />
+        </div>
+        <Button 
+          size='sm' 
+          className='rounded-sm py-1.5 px-2 h-auto'
+        >
+          <span className='hidden md:block'>Create</span>
+          <Plus className='block pl-1 h-4 w-4'/>
+        </Button>
+      </div>
+    </nav>
   );
 };
