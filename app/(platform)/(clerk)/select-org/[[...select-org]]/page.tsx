34da22486f04ac5658b188a3baee129583b16cab
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-16 23:20:21 -0800

    Allow visionize to be used for both personal and organizations
    This commit adds three props to the OrganizationList component from @clerk/nextjs: afterCreateOrganizationUrl, afterSelectPersonalUrl, and afterSelectOrganizationUrl. These props allow the user to to navigate to after creating or selecting an organization or a personal account. This design decision enables both teams and individuals.

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -3,6 +3,10 @@ import { OrganizationList } from '@clerk/nextjs';
 
 export default function CreateOrganizationPage() {
   return (
-    <OrganizationList />
+    <OrganizationList 
+      afterCreateOrganizationUrl='/organization/:id'
+      afterSelectPersonalUrl='/user/:id'
+      afterSelectOrganizationUrl='/organization/:id'
+    />
   );
 };
