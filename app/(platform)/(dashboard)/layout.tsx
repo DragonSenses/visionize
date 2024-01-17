10c4a92bf9275d7e83f5c142eac1eada8069edaf
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-17 04:40:22 -0800

    Add navbar to dashboard layout

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Navbar } from './_components/navbar';
 
 const DashboardLayout = ({children}: {
   children: React.ReactNode;
 }) => {
   return (
     <div className='h-full'>
-      {/* Navbar */}
+      <Navbar />
       {children}
     </div>
   )
