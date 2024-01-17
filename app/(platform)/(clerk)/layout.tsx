18995331a986fadabb20a37f3ffd6f05ddebe313
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-16 21:24:00 -0800

    Add layout component for clerk pages
    Create a nested layout called ClerkLayout that renders a centered flex container for clerk pages. This component can be used to wrap other components that need to be aligned in the center of the screen.

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ClerkLayout = ({ children}:{ 
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className='h-full flex items-center justify-center'>
+      {children}
+    </div>
+  );
+};
+
+export default ClerkLayout;
