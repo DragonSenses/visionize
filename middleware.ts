06d9a2652735f174202990511b911d5c98eeb151
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-18 23:02:19 -0800

    Handle unauthenticated users in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,26 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+
 export default authMiddleware({
   publicRoutes: ["/"],
+
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      } else {
+        path = `/user/${auth.userId}`;
+      }
+    }
+  },
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
