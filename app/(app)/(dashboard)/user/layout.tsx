9b3898b81cc5dcfc3a2afcc6a4b502ef72f6c4ed
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-20 17:35:14 -0800

    Create UserLayout

---
+++ app/(app)/(dashboard)/user/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
