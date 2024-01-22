12e3ca4ab6a1b682a50c19ac9883472bc44237e6
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-21 18:51:31 -0800

    Create OrganizationIdLayout

---
+++ app/(app)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function OrganizationIdLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
