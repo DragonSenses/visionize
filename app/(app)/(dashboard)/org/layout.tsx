92a9e5ebb4e07173f482a3fbb3cf2fb893319d68
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-22 05:42:41 -0800

    Add sidebar to org layout

--- app/(app)/(dashboard)/org/layout.tsx
+++ app/(app)/(dashboard)/org/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Sidebar from '../_components/Sidebar';
 
 export default function OrganizationLayout({
   children,
@@ -9,6 +10,7 @@ export default function OrganizationLayout({
     <main className='px-4 pt-20 md:pt-24 mx-auto max-w-6xl 2xl:max-w-screen-xl'>
       <div className='flex '>
         <div className='w-64 shrink-0 hidden md:block'>
+          <Sidebar />
         </div>
         {children}
       </div>
