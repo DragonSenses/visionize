f768d2cf5ea62ef35169058ceb11bafe7576da04
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-21 18:14:21 -0800

    Style organization layout

--- app/(app)/(dashboard)/organization/layout.tsx
+++ app/(app)/(dashboard)/organization/layout.tsx
@@ -6,8 +6,12 @@ export default function OrganizationLayout({
   children: React.ReactNode
 }) {
   return (
-    <main>
-      {children}
+    <main className='px-4 pt-20 md:pt-24 mx-auto max-w-6xl 2xl:max-w-screen-xl'>
+      <div className='flex '>
+        <div className='w-64 shrink-0 hidden md:block'>
+        </div>
+        {children}
+      </div>
     </main>
   )
 }
