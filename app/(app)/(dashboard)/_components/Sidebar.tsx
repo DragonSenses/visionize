22256a422c62191f0899dcef445c3f12de7e6acf
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-23 21:21:15 -0800

    Add imports to sidebar component

--- app/(app)/(dashboard)/_components/Sidebar.tsx
+++ app/(app)/(dashboard)/_components/Sidebar.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import Link from 'next/link';
 import React from 'react'
+import { Plus } from 'lucide-react';
+import { useLocalStorage } from 'usehooks-ts';
+import { useOrganization, useOrganizationList } from '@clerk/nextjs';
+
+import { Accordion } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface SidebarProps {
   key?: string;
@@ -10,6 +19,8 @@ export default function Sidebar({
   key,
 }: SidebarProps ) {
   return (
-    <div>Sidebar</div>
+    <div>
+      Sidebar
+    </div>  
   );
 };
