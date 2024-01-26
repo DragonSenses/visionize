03e494a7cc133268ee5d098626626981d4cf54eb
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-25 20:05:17 -0800

    Add AccordionItem and trigger to output

--- app/(app)/(dashboard)/_components/SidebarItem.tsx
+++ app/(app)/(dashboard)/_components/SidebarItem.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
 
+import { cn } from '@/lib/utils';
 import Organization from '@/types/organization';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 interface SidebarItemProps {
   isActive: boolean;
@@ -16,6 +24,24 @@ export default function SidebarItem({
   organization,
 }: SidebarItemProps ) {
   return (
-    <div>SidebarItem</div>
+    <AccordionItem
+      value={organization.id}
+      className='border-none'
+    >
+      <AccordionTrigger
+        onClick={() => onOpen(organization.id)}
+        className={cn()}
+      >
+        <div className=''>
+          <div className=''>
+            <Image 
+            />
+          </div>
+          <span className=''>
+            {organization.name}
+          </span>
+        </div>
+      </AccordionTrigger>
+    </AccordionItem>
   )
 }
