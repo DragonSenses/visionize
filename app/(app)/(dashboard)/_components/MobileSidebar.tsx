46655ccb83803b873c29caf31946b090f2f59aa1
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-01 08:02:11 -0800

    Add imports, state and actions in MobileSidebar

--- app/(app)/(dashboard)/_components/MobileSidebar.tsx
+++ app/(app)/(dashboard)/_components/MobileSidebar.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+"use client";
+
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+import { useMobileSidebar } from '@/hooks/useMobileSidebar';
 
 export default function MobileSidebar() {
+  // Get the current path of the page
+  const pathname = usePathname();
+
+  /* These values are used to control the visibility and behavior 
+  of the mobile sidebar component */
+  const isOpen = useMobileSidebar((state) => state.isOpen);
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onClose);
+
   return (
     <div>MobileSidebar</div>
   )
