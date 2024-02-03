b61014582d563604987e9bad6c9ee1c4cd773505
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-02 20:18:39 -0800

    Modify styles of OrganizationLayout

--- app/(app)/(dashboard)/org/layout.tsx
+++ app/(app)/(dashboard)/org/layout.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Sidebar from '../_components/Sidebar';
 
+// Add following styles to <main> to limit the width on larger screens
+// max-w-6xl 2xl:max-w-screen-xl
+
 export default function OrganizationLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <main className='px-4 pt-20 md:pt-24 mx-auto max-w-6xl 2xl:max-w-screen-xl'>
-      <div className='flex '>
+    <main className='px-4 pt-20 md:pt-24 mx-auto'>
+      <div className='flex gap-x-7'>
         <div className='w-64 shrink-0 hidden md:block'>
           <Sidebar />
         </div>
