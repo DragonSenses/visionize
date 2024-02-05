95261094253bd7856b89df765eb6b368d9ba8be6
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-04 23:41:45 -0800

    Add a server action to create a board in the db

--- app/(app)/(dashboard)/org/[orgId]/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/page.tsx
@@ -1,12 +1,32 @@
-import { auth } from '@clerk/nextjs';
 import React from 'react';
+import { auth } from '@clerk/nextjs';
 
 const OrganizationIdPage = () => {
   const { userId, orgId } = auth();
 
+  async function create(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+
+    await database.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
   return (
     <div>
-      {`Organization: ${orgId}`}
+      <form action={create}>
+        <input 
+          id='title'
+          name='title'
+          placeholder='Enter a board title'
+          required
+          className='border-black border p-1'
+        />
+      </form>
     </div>
   );
 };
