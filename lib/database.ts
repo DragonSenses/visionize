dacca617719e1604585b2860d29dd8724879ffad
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-05 21:26:46 -0800

    Update utility to query with global prisma client

--- lib/database.ts
+++ lib/database.ts
@@ -1,7 +1,34 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+/** 
+ * Instantiate Prisma Client by defining a global prisma instance.
+ *  
+ * This code is a way to prevent creating multiple instances of 
+ * Prisma Client in your application, which can lead to performance
+ * issues or errors.
+*/
 
+// Declare a global variable prisma of type PrismaClient or undefined
+declare global {
+  var prisma: PrismaClient | undefined;
+};
+
+// Export a database variable that is either the existing global prisma instance or a new one
+export const database = globalThis.prisma || new PrismaClient();
+
+// If the environment is not production, assign the database variable to the global prisma variable
+if(process.env.NODE_ENV !== "production") {
+  globalThis.prisma = database;
+}
+
+/**
+ * Prisma Client - Querying the database
+ * @see https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/querying-the-database-typescript-postgresql
+ */
+
+// const prisma = new PrismaClient()
+
+/*  
 async function main() {
   // ... you will write your Prisma Client queries here
 }
@@ -15,3 +42,5 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+*/
