0e1ab5e86f0c28b7c7da3b8e69797150338c9527
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-05 22:25:36 -0800

    refactor(board): validate user input with zod
    Use Zod to define and validate the structure and constraints of the title field. Zod can also infer the TypeScript types from the schemas, eliminating the need for duplicating type declarations. Zod improves the readability, maintainability, and performance of the validation logic.

--- actions/createBoard.ts
+++ actions/createBoard.ts
@@ -1,9 +1,17 @@
 "use server";
 
+import { z } from "zod";
+
 import { database } from "@/lib/database";
 
+const CreateBoard = z.object({
+  title: z.string(),
+});
+
 export default async function createBoard(formData: FormData) {
-  const title = formData.get("title") as string;
+  const { title } = CreateBoard.parse({
+    title: formData.get("title"),
+  });
 
   await database.board.create({
     data: {
