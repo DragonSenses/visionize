b0ee47b1dc573104ed8322abeec46c06ed04804e
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-06 23:22:47 -0800

    Add server action to update a board

---
+++ actions/updateBoard.ts
@@ -0,0 +1,21 @@
+import { revalidatePath } from 'next/cache';
+
+import { database } from '@/lib/database';
+
+interface BoardData {
+  title: string;
+};
+
+export default async function updateBoard(id: string, boardData: BoardData) {
+
+  await database.board.update({
+    where: {
+      id: id,
+    },
+    data: {
+      title: boardData.title
+    }
+  });
+
+  revalidatePath('/org/org_yourOrgIdHere');
+}
