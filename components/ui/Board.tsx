35e0e15fa67d382e63967b0a1feeba4d66aa6ef0
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-07 07:15:39 -0800

    Add update and delete handlers

--- components/ui/Board.tsx
+++ components/ui/Board.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
+import deleteBoard from '@/actions/deleteBoard';
+import updateBoard from '@/actions/updateBoard';
 
 interface BoardProps {
   id: string;
+  title: string;
+};
+
+interface BoardData {
   title: string;
 };
 
@@ -10,6 +16,17 @@ export default function Board({
   id,
   title,
 }: BoardProps) {
+  
+  const handleDelete = async () => {
+    console.log('Deleting board with id:', id);
+    await deleteBoard(id);
+  };
+  
+  const handleUpdate = async (boardData: BoardData) => {
+    console.log('Updating board with id:', id);
+    await updateBoard(id, boardData);
+  };
+
   return (
     <form className='flex items-center gap-x-2'>
       <p>{title}</p>
@@ -18,6 +35,7 @@ export default function Board({
         type="submit"
         variant="default"
         size="sm"
+        onClick={handleUpdate({title: title})}
       >
         Update
       </Button>
@@ -25,6 +43,7 @@ export default function Board({
         type="submit"
         variant="destructive"
         size="sm"
+        onClick={handleDelete}
       >
         Delete
       </Button>
