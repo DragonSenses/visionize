a1027b0e164fe4ebd24c2dc4fbedfc7aa0670d9e
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-06 22:49:28 -0800

    Render each board as a Board component

--- app/(app)/(dashboard)/org/[orgId]/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/page.tsx
@@ -3,14 +3,17 @@ import { auth } from '@clerk/nextjs';
 import createBoard from '@/actions/createBoard';
 import { Button } from '@/components/ui/button';
 import { database } from '@/lib/database';
+import Board from '@/components/ui/Board';
 
 const OrganizationIdPage = async () => {
+  // Fetch the boards from the database
   const boards = await database.board.findMany();
 
   return (
     <div className='flex flex-col space-y-4'>
+      {/* Create a form for creating a new board */}
       <form action={createBoard}>
-        <input 
+        <input
           id='title'
           name='title'
           placeholder='Enter a board title'
@@ -18,14 +21,19 @@ const OrganizationIdPage = async () => {
           className='border-black border p-1'
         />
       </form>
+      {/* Submit Button */}
       <Button type='submit'>
         Submit
       </Button>
+      {/* Create a div for displaying the boards */}
       <div className='space-y-2'>
+        {/* Map over the boards and render a Board component for each one */}
         {boards.map((board) => (
-          <div key={board.id}>
-            {board.title}
-          </div>
+          <Board
+            key={board.id}
+            id={board.id}
+            title={board.title}
+          />
         ))}
       </div>
     </div>
