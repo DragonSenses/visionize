164e9459cf99cb586b65ee6926efdc29922206fa
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-07 09:11:30 -0800

    Add form control in BoardForm with useFormState

--- components/BoardForm.tsx
+++ components/BoardForm.tsx
@@ -1,14 +1,29 @@
 "use client";
 
 import React from 'react';
+import { useFormState } from 'react-dom';
 
 import createBoard from '@/actions/createBoard';
 import { Button } from '@/components/ui/button';
 
 /* Create a form for creating a new board */
 export default function BoardForm() {
+
+  const initialState = {
+    message: null,
+    errors: {},
+  };
+
+  // Use the useFormState hook to create a state and an action for the form
+  const [state, formAction] = useFormState(createBoard, initialState);
+  
+  /* The state will be updated by the createBoard action when the form is submitted
+  The createBoard action is a function that takes the previous state and the form 
+  data as arguments and returns a new state */
+
+  // Pass the formAction as the action prop to the form element
   return (
-    <form action={createBoard}>
+    <form action={formAction}>
       <input
         id='title'
         name='title'
