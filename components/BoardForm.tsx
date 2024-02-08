52b3addb5fbbb1cd4c30ef02c777a0a793fc1745
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-08 13:52:13 -0800

    feat: render message if state has errors

--- components/BoardForm.tsx
+++ components/BoardForm.tsx
@@ -10,13 +10,13 @@ import { Button } from '@/components/ui/button';
 export default function BoardForm() {
 
   const initialState = {
-    message: null,
     errors: {},
+    message: "",
   };
 
   // Use the useFormState hook to create a state and an action for the form
   const [state, formAction] = useFormState(createBoard, initialState);
-  
+
   /* The state will be updated by the createBoard action when the form is submitted
   The createBoard action is a function that takes the previous state and the form 
   data as arguments and returns a new state */
@@ -24,13 +24,24 @@ export default function BoardForm() {
   // Pass the formAction as the action prop to the form element
   return (
     <form action={formAction}>
-      <input
-        id='title'
-        name='title'
-        placeholder='Enter a board title'
-        required
-        className='border-black border p-1'
-      />
+      <div className="flex flex-col space-y-2">
+        <input
+          id='title'
+          name='title'
+          placeholder='Enter a board title'
+          required
+          className='border-black border p-1'
+        />
+        {state?.errors?.title ? (
+           <div>
+              {state.errors.title.map((error: string) => (
+                <p key={error} className='text-rose-500'>
+                  {error}
+                </p>
+              ))}
+           </div>
+        ) : null}
+      </div>
       <Button type='submit'>
         Submit
       </Button>
