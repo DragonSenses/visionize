1797cc14ece99706b027012f57f73ba4c7a95df6
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-09 11:32:43 -0800

    Parameterize BoardFormButton component
    The BoardFormButton component was previously hard-coded to render a submit button with the text "Submit". This commit adds props to the component to allow customization of the button content, size, type, variant, and onClick handler. This makes the component more re-usable and flexible for different use cases.

--- components/BoardFormButton.tsx
+++ components/BoardFormButton.tsx
@@ -4,13 +4,34 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { useFormStatus } from 'react-dom';
 
-export default function BoardFormButton() {
+interface BoardFormButtonProps {
+  children: React.ReactNode;
+  size?: "default" | "sm" | "lg" | "icon" | null | undefined;
+  type: "submit" | "reset" | "button";
+  variant?: "default" | "destructive" | "outline" | "secondary" | 
+            "ghost" | "link" | "primary" | null | undefined;
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+}
+
+export default function BoardFormButton({
+  children,
+  size,
+  type,
+  variant,
+  onClick,
+}: BoardFormButtonProps ) {
 
   const { pending } = useFormStatus();
 
   return (
-    <Button disabled={pending} type='submit'>
-      Submit
+    <Button 
+      disabled={pending} 
+      type={type} 
+      variant={variant} 
+      size={size} 
+      onClick={onClick}
+    >
+      {children}
     </Button>
   )
 }
