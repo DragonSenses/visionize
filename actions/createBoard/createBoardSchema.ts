1cc1a4bd20fdfe9f7f62e264b98d04d45ee7c9d2
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-10 06:53:59 -0800

    Create createBoardSchema.ts
    Encapsulate the validation rules specific to creating a board. This uses the zod library to define validation schemes.

---
+++ actions/createBoard/createBoardSchema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+/**
+ * Define the CreateBoard object schema.
+ * 
+ * Add custom error messages for: required fields, 
+ * invalid type and minimum length.
+ */
+export const CreateBoard = z.object({
+  title: z.string({
+    required_error: "Title is required", 
+    invalid_type_error: "Title is required", 
+  }).min(3, {
+    message: "Must be 3 or more characters long.", 
+  }),
+});
