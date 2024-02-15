e1fef46579d5971f30dd901843ddeaad37a91457
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-14 18:39:13 -0800

    refactor: replace useFormState with useServerAction
    This commit replaces the useFormState hook with the useServerAction hook in the BoardForm component. The useServerAction hook simplifies the logic and state management of the server action by handling the data, error, field errors, and loading status internally. The BoardForm component only needs to pass the createBoard action and the input to the executeServerAction function returned by the hook. This improves the readability and maintainability of the code and the component usage.

--- components/BoardForm.tsx
+++ components/BoardForm.tsx
@@ -1,31 +1,25 @@
 "use client";
 
 import React from 'react';
-import { useFormState } from 'react-dom';
 
-import createBoard from '@/actions/createBoard';
+import { createBoard } from "@/actions/createBoard/index";
 import BoardFormInput from '@/components/BoardFormInput';
 import BoardFormButton from '@/components/BoardFormButton';
+import { useServerAction } from '@/hooks/useServerAction';
 
 /* Create a form for creating a new board */
 export default function BoardForm() {
+  const { executeServerAction, fieldErrors } = useServerAction(createBoard);
 
-  const initialState = {
-    errors: {},
-    message: "",
-  };
+  function onSubmit(formData: FormData) {
+    const title = formData.get('title') as string;
 
-  // Use the useFormState hook to create a state and an action for the form
-  const [state, formAction] = useFormState(createBoard, initialState);
+    executeServerAction({ title });
+  }
 
-  /* The state will be updated by the createBoard action when the form is submitted
-  The createBoard action is a function that takes the previous state and the form 
-  data as arguments and returns a new state */
-
-  // Pass the formAction as the action prop to the form element
   return (
-    <form action={formAction}>
-      <BoardFormInput errors={state?.errors}/>
+    <form action={onSubmit}>
+      <BoardFormInput errors={fieldErrors}/>
       <BoardFormButton type="submit" variant="default" size="default">
         Submit
       </BoardFormButton>
