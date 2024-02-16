b2d461881ac5f49cdbdac9bd53824779411c0235
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-15 16:28:16 -0800

    Add FormInputProps interface

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export default function FormInput() {
+interface FormInputProps {
+  id: string;
+  className?: string;
+  defaultValue?: string;
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  disabled?: boolean;
+  required?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  onBlur?: () => void;
+}
+
+const FormInput = {
   return (
     <div>FormInput</div>
   )
 }
+
+export default FormInput;
