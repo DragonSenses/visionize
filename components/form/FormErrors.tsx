8fd11baac0d20e46dd3d9514a8af890502eaf52d
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-16 13:11:49 -0800

    Implement FormErrors component
    Use id prop to link input and error elements.  Use aria-live attribute to announce errors to screen readers. Use XCircle icon from lucide-react to display errors for each input.

--- components/form/FormErrors.tsx
+++ components/form/FormErrors.tsx
@@ -1,7 +1,35 @@
+import { XCircle } from 'lucide-react';
 import React from 'react'
 
-export default function FormErrors() {
+interface FormErrorsProp {
+  id: string;
+  errors?: Record<string, string[] | undefined>;
+};
+
+export default function FormErrors({
+  id, 
+  errors,
+}: FormErrorsProp) {
+  
+  if (!errors) {
+    return null;
+  }
+
   return (
-    <div>FormErrors</div>
+    <div
+      id={`${id}-error`}
+      aria-live='polite'
+      className='mt-2 text-xs text-rose-500'
+    >
+      {errors?.[id]?.map((error: string) => (
+        <div
+          key={error}
+          className='flex items-center rounded-sm font-medium p-2 border border-rose-500 bg-rose-500/10'
+        >
+          <XCircle className='h-4 w-4 mr-2'/>
+          {error}
+        </div>
+      ))}
+    </div>
   )
 }
