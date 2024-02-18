c11f57a43d5a5615910d38f459e6b7d2b1f9c032
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-17 19:22:20 -0800

    feat: add size prop to FormSubmitButton
    Add size prop to FormSubmitButton component to allow different button sizes. The size prop can be one of 'default', 'sm', 'lg', or 'icon'. The component also uses the useFormStatus hook to disable the button when the form is pending. Also add cn utility function to merge Tailwind CSS class names.

--- components/form/FormSubmitButton.tsx
+++ components/form/FormSubmitButton.tsx
@@ -10,6 +10,7 @@ interface FormSubmitProps {
   children: React.ReactNode;
   className?: string;
   disabled?: boolean;
+  size: 'default' | 'sm' | 'lg' | 'icon';
   variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' 
     | 'link' | 'primary';
 };
@@ -18,6 +19,7 @@ export default function FormSubmitButton({
   children,
   className,
   disabled,
+  size,
   variant,
 }: FormSubmitProps) {
   const { pending } = useFormStatus();
@@ -25,7 +27,7 @@ export default function FormSubmitButton({
   return (
     <Button
       disabled={pending || disabled}
-      size='sm'
+      size={size}
       type='submit'
       variant={variant}
       className={cn(className)}
