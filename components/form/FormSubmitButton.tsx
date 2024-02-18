045854b594757fa003cba9fb0e417518f0646554
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-17 19:21:10 -0800

    Update output of FormSubmitButton

--- components/form/FormSubmitButton.tsx
+++ components/form/FormSubmitButton.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import React from 'react';
+import { useFormStatus } from 'react-dom';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
 interface FormSubmitProps {
   children: React.ReactNode;
@@ -16,7 +20,17 @@ export default function FormSubmitButton({
   disabled,
   variant,
 }: FormSubmitProps) {
+  const { pending } = useFormStatus();
+
   return (
-    <div>FormSubmitButton</div>
+    <Button
+      disabled={pending || disabled}
+      size='sm'
+      type='submit'
+      variant={variant}
+      className={cn(className)}
+    >
+      {children}
+    </Button>
   )
 }
