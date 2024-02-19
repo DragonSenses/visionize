111351195ed9ad4bf2903d5fb81f3f45c7681677
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-18 21:11:51 -0800

    refactor: org ID page with BoardList & Info

--- app/(app)/(dashboard)/org/[orgId]/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/page.tsx
@@ -1,25 +1,16 @@
+import BoardList from '@/components/BoardList';
+import Info from '@/components/Info';
+import { Separator } from '@/components/ui/separator';
 import React from 'react';
-import { database } from '@/lib/database';
-import Board from '@/components/Board';
-import BoardForm from '@/components/BoardForm';
 
-const OrganizationIdPage = async () => {
-  // Fetch the boards from the database
-  const boards = await database.board.findMany();
+const OrganizationIdPage = () => {
 
   return (
-    <div className='flex flex-col space-y-4'>
-      <BoardForm />
-      {/* Create a div for displaying the boards */}
-      <div className='space-y-2'>
-        {/* Map over the boards and render a Board component for each one */}
-        {boards.map((board) => (
-          <Board
-            key={board.id}
-            id={board.id}
-            title={board.title}
-          />
-        ))}
+    <div className='flex flex-col w-full mb-20'>
+      <Info />
+      <Separator className='my-4'/>
+      <div className='px-2 md:px-4'>
+        <BoardList />
       </div>
     </div>
   );
