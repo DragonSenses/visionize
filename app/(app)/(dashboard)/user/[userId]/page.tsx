9fe9d60d082bfdc002d9a79021edb2828b8a9c76
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-18 20:22:04 -0800

    feat: display boards from database in UserIdPage

--- app/(app)/(dashboard)/user/[userId]/page.tsx
+++ app/(app)/(dashboard)/user/[userId]/page.tsx
@@ -1,14 +1,27 @@
-import { auth } from '@clerk/nextjs';
 import React from 'react';
 
-const UserIdPage = () => {
-  const { userId } : { userId: string | null } = auth();
+import { database } from '@/lib/database';
+import Board from '@/components/Board';
+import BoardForm from '@/components/BoardForm';
+
+export default async function UserIdPage () {
+  // Fetch the boards from the database
+  const boards = await database.board.findMany();
 
   return (
-    <div>
-      `User: ${userId}`
+    <div className='flex flex-col space-y-4'>
+      <BoardForm />
+      {/* Create a div for displaying the boards */}
+      <div className='space-y-2'>
+        {/* Map over the boards and render a Board component for each one */}
+        {boards.map((board) => (
+          <Board
+            key={board.id}
+            id={board.id}
+            title={board.title}
+          />
+        ))}
+      </div>
     </div>
   );
 };
-
-export default UserIdPage
