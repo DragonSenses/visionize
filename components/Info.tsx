c3715ed20457fa0d226e41a8d5b5714c3b8cc6aa
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-18 21:22:23 -0800

    feat: display organization image in Info component

--- components/Info.tsx
+++ components/Info.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import Image from 'next/image';
+import { useOrganization } from '@clerk/nextjs';
 
 export default function Info() {
+  const { organization, isLoaded } = useOrganization();
+
+  if (!isLoaded) {
+    return (
+      <p>Loading...</p>
+    )
+  }
+
   return (
-    <div>
-      Info
+    <div className='flex items-center gap-x-4'>
+      {/* Image container */}
+      <div className='relative w-[60px] h-[60px]'>
+        <Image
+          fill
+          src={organization?.imageUrl}
+          alt="organization image"
+          className='rounded-md object-cover'
+          sizes="(max-width: 768px) 33vw, (max-width: 1200px) 30vw, 25vw"
+        />
+      </div>
     </div>
   )
 }
