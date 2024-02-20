65906743a4f62c0ca8ab41a5a5bac9fb214b878e
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-19 19:36:24 -0800

    Add icon and responsive grid to BoardList

--- components/BoardList.tsx
+++ components/BoardList.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { UserRound } from 'lucide-react';
 
 export default function BoardList() {
+  // Fetch boards here
+
   return (
-    <div>
-      BoardList
+    <div className='space-y-4'>
+      {/* Board list header */}
+      <div className='flex items-center text-lg text-neutral-700 font-semibold'>
+        <UserRound className='h-6 w-6 mr-2' />
+        Your boards
+      </div>
+      {/* Grid of boards */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
+
+      </div>
+
     </div>
   )
 }
