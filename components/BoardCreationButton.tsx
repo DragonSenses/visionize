c64d744f258c039369cc888828b41a338c8c7da6
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-19 19:43:12 -0800

    Update output for BoardCreationButton

--- components/BoardCreationButton.tsx
+++ components/BoardCreationButton.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function BoardCreationButton() {
   return (
-    <button>
-      BoardCreationButton
-    </button>
+    <Button
+      className='relative flex flex-col items-center h-full w-full rounded-sm aspect-video bg-muted gap-y-1 justify-center transition hover:opacity-75'
+    >
+      <p className='text-sm'>Create new board</p>
+      <span className='text-xs'>
+        15 remaining
+      </span>
+    </Button>
   )
 }
