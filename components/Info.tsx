65ca9ee010d9631d2008a473c9d9e0e2409736a7
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-19 16:36:11 -0800

    Add skeleton loading to Info
    Use the useOrganization hook from Clerk to fetch the organization data and show a loading state with the Info.Skeleton component. The Info.Skeleton component creates a placeholder preview of the Info content before it fully loads. This improves the user experience and reduces the perceived loading time.

--- components/Info.tsx
+++ components/Info.tsx
@@ -5,12 +5,14 @@ import Image from 'next/image';
 import { useOrganization } from '@clerk/nextjs';
 import { CreditCard } from 'lucide-react';
 
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function Info() {
   const { organization, isLoaded } = useOrganization();
 
   if (!isLoaded) {
     return (
-      <p>Loading...</p>
+      <Info.Skeleton />
     )
   }
 
@@ -35,6 +37,35 @@ export default function Info() {
         <div className='flex items-center text-xs text-muted-foreground'>
           <CreditCard />
           Free
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * Shows a placeholder preview of the content before it fully loads. Improves
+ * user experience by showing a layout & structure of the component content,
+ * but withoout the details. Reduces the perceived loading time and keeps the
+ * user engaged.
+ * 
+ * Uses React syntax to define a static property on the Info component and 
+ * assign the function component which renders the skeleton.
+ * @returns the skeleton of the Info component
+ */
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className='flex items-center gap-x-4'>
+      {/* Image container */}
+      <div className='relative w-[60px] h-[60px]'>
+        <Skeleton className='absolute w-full h-full' />
+      </div>
+      {/* Organization Info */}
+      <div className='space-y-2'>
+        <Skeleton className='h-10 w-[200px]' />
+        <div className='flex items-center'>
+          <Skeleton className='h-4 w-4 mr-2' />
+          <Skeleton className='h-4 w-[100px]' />
         </div>
       </div>
     </div>
