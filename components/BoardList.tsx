6b4e8849f06dec831d56e62fac1f91d469d0a79d
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-19 19:40:23 -0800

    feat: add BoardCreationButton in BoardList

--- components/BoardList.tsx
+++ components/BoardList.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { UserRound } from 'lucide-react';
+import BoardCreationButton from '@/components/BoardCreationButton';
 
 export default function BoardList() {
   // Fetch boards here
 
   return (
     <div className='space-y-4'>
-      {/* Board list header */}
+      {/* User icon header */}
       <div className='flex items-center text-lg text-neutral-700 font-semibold'>
         <UserRound className='h-6 w-6 mr-2' />
         Your boards
       </div>
       {/* Grid of boards */}
       <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
-
+        <BoardCreationButton />
       </div>
 
     </div>
