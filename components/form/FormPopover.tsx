310472d3543e6b29beddcf11cab1cef03e39a426
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-21 15:34:22 -0800

    feat: Add more props to FormPopover component
    Additional props to the FormPopover component are now available:
    
    - `align`: Specifies the alignment of the popover content (options: 'start', 'center', 'end').
    - `sideOffset`: Allows adjusting the distance between the popover and its target element.
    - `side`: Determines the side of the target element where the popover should appear (options: 'top', 'right', 'bottom', 'left').
    
    These props enhance the flexibility and customization options for the FormPopover component.

--- components/form/FormPopover.tsx
+++ components/form/FormPopover.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 interface FormPopoverProps {
   children: React.ReactNode;
-}
+  align?: 'start' | 'center' | 'end';
+  sideOffset?: number;
+  side?: 'top' | 'right' | 'bottom' | 'left';
+};
 
 export default function FormPopover({
   children,
+  align,
+  sideOffset = 0,
+  side = 'bottom',
 }: FormPopoverProps) {
   return (
     <div>
