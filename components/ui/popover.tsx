a7a7015618204ed3858f5da70691465e103cad65
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-21 18:36:24 -0800

    feat: extend Popover with close functionality
    This commit enhances the Popover component by adding the PopoverClose component, allowing users to close the popover when needed.

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -9,6 +9,9 @@ const Popover = PopoverPrimitive.Root
 
 const PopoverTrigger = PopoverPrimitive.Trigger
 
+// Add close functionality to the Popover
+const PopoverClose = PopoverPrimitive.Close
+
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
@@ -28,4 +31,4 @@ const PopoverContent = React.forwardRef<
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
-export { Popover, PopoverTrigger, PopoverContent }
+export { Popover, PopoverTrigger, PopoverContent, PopoverClose }
