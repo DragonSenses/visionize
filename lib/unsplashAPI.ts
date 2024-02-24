6b63378d80ec0e9eee2d995ce12247daa9ba0727
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-23 21:09:22 -0800

    Create unsplashAPI.ts

---
+++ lib/unsplashAPI.ts
@@ -0,0 +1,30 @@
+import { createApi } from 'unsplash-js';
+
+/* Create API object from unsplash-js library, to access and manipulate photos
+from https://unsplash.com
+ */
+export const unsplashApi = createApi({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY,
+  fetch: fetch,
+});
+
+
+/** 
+ * 
+ * @see https://github.com/unsplash/unsplash-js?tab=readme-ov-file#usage
+*/
+/* 
+
+// on your node server
+const serverApi = createApi({
+  accessKey: 'MY_ACCESS_KEY',
+  //...other fetch options
+});
+
+// in the browser
+const browserApi = createApi({
+  apiUrl: 'https://mywebsite.com/unsplash-proxy',
+  //...other fetch options
+});
+
+*/
