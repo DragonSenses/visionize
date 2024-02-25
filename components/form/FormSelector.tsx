4414f92bd0da7468e2257445371c33f99fa04883
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-24 17:19:37 -0800

    feat: integrate Unsplash API with FormSelector
    - Use unsplashApi to fetch random images from collection 317099
    - Use useEffect hook to fetch images on component mount
    - Use useState hook to store images in local state
    - Display images in FormSelector component

--- components/form/FormSelector.tsx
+++ components/form/FormSelector.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { unsplashApi } from '@/lib/unsplashAPI';
 
 interface FormPickerProps {
   id: string;
@@ -11,6 +12,33 @@ export default function FormSelector({
   id,
   errors,
 }: FormPickerProps) {
+  const [images, setImages] = useState<Array<Record<string, any>>>([]);
+
+  // Fetch images with useEffect
+  useEffect(() => {
+    // Fetch images from collection 317099, curated by Unsplash Editorial
+    const fetchImages = async () => {
+      try {
+        const result = await unsplashApi.photos.getRandom({
+          collectionIds: ["317099"],
+          count: 9,
+        });
+        
+        if (result && result.response) {
+          const imageData = (result.response as Array<Record<string, any>>);
+          setImages(imageData);
+        } else {
+          console.error("Failed to fetch images from Unsplash.")
+        }
+
+      } catch(error) {
+        console.log(error);
+        // Reset images array
+        setImages([]);
+      }
+    }
+  });
+
   return (
     <div>FormSelector</div>
   )
