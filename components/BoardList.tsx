0632719e842e9202c499603e6f6fb3c4c6bdaed3
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-03 20:39:37 -0800

    style: Add darken effect to board thumbnails
    In the `BoardList` component, each board thumbnail is now displayed with a dark overlay on hover. The background image (`imageThumbUrl`) remains visible, but the overlay enhances readability of the board title.

--- components/BoardList.tsx
+++ components/BoardList.tsx
@@ -37,9 +37,13 @@ export default async function BoardList() {
           <Link
             key={board.id}
             href={`/board/${board.id}`}
+            className='group relative h-full w-full p-2 aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm overflow-hidden'
             style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
           >
-            <p>
+            <div
+              className='absolute inset-0 bg-black/30 group-hover:bg-black/40 transition'
+            />
+            <p className='relative font-semibold text-white'>
               {board.title}
             </p>
           </Link>
