cb4e2cdc5d70ffe9380d851040c0274c89972d87
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-03 21:06:13 -0800

    feat: Suspend BoardList in Org ID page
    In the `OrganizationIdPage`, the `BoardList` component is now wrapped in a `Suspense` fallback. When loading, it displays a skeleton placeholder to enhance user experience.

--- app/(app)/(dashboard)/org/[orgId]/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/page.tsx
@@ -1,16 +1,19 @@
+import React, { Suspense } from 'react';
+
 import BoardList from '@/components/BoardList';
 import Info from '@/components/Info';
 import { Separator } from '@/components/ui/separator';
-import React from 'react';
 
 const OrganizationIdPage = () => {
 
   return (
     <div className='flex flex-col w-full mb-20'>
       <Info />
-      <Separator className='my-4'/>
+      <Separator className='my-4' />
       <div className='px-2 md:px-4'>
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
