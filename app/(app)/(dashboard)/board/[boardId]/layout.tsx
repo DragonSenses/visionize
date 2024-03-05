774263dbd930d589a010f4df9d5cda1f3fb881e0
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-04 20:38:36 -0800

    Create BoardIdLayout

---
+++ app/(app)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function BoardIdLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
