2781ab2efae90b78001fb35eb330812002387504
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-09 18:33:03 -0800

    Update index.ts

--- actions/updateBoard/index.ts
+++ actions/updateBoard/index.ts
@@ -3,5 +3,18 @@
 import { InputType, OutputType } from "./updateBoardTypes";
 
 async function performAction (data: InputType): Promise<OutputType> {
-  
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { title, id } = data;
+
+  // Return the updated board
+  return {
+
+  };
 }
