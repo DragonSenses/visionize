e7fcdb815f66654fbc20721f08d10e252a0554fd
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-10 22:53:54 -0700

    Create BoardOptions.tsx

---
+++ app/(app)/(dashboard)/board/[boardId]/_components/BoardOptions.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+interface BoardOptionsProps {
+  id: string;
+};
+
+export default function BoardOptions({ id }: BoardOptionsProps) {
+  return (
+    <div>BoardOptions</div>
+  )
+}
