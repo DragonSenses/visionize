bfa83b22232be567eb18cac14639366b781c0749
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-10 21:41:13 -0700

    feat: Implement update board as server action

--- actions/updateBoard/index.ts
+++ actions/updateBoard/index.ts
@@ -1,6 +1,9 @@
 "use server";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
 
 import { createServerAction } from "@/lib/createServerAction";
+import { database } from "@/lib/database";
 
 import { UpdateBoard } from "./updateBoardSchema";
 import { InputType, OutputType } from "./updateBoardTypes";
@@ -16,8 +19,30 @@ async function performAction (data: InputType): Promise<OutputType> {
 
   const { title, id } = data;
 
+  let board;
+
+  try {
+    board = await database.board.update({
+      where: {
+        id,
+        orgId,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to update board.'
+    }
+  }
+
+  revalidatePath(`/board/${id}`);
+
   // Return the updated board
-  return { };
+  return {
+    data: board
+  };
 }
 
 export const updateBoard = createServerAction(UpdateBoard, performAction);
