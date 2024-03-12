1b3b1880094e210ea7280a5267027665c14988e1
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-11 20:55:45 -0700

    feat: Use BoardOptions component in BoardNavbar

--- app/(app)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
+++ app/(app)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Board } from '@prisma/client';
 import BoardTitleForm from './BoardTitleForm';
+import BoardOptions from './BoardOptions';
 
 interface BoardNavbarProps {
   data: Board;
@@ -13,6 +14,11 @@ export default async function BoardNavbar({
   return (
     <div className='flex items-center fixed h-14 w-full top-14 z-[30] bg-black/50 px-6 gap-x-4 text-white'>
       <BoardTitleForm data={data} />
+      <div className='ml-auto'>
+        <BoardOptions
+          id={data.id}
+        />
+      </div>
     </div>
   )
 }
