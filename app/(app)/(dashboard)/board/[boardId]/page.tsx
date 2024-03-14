296d132bc7f89575e56d87a6dafc5c9785db3286
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-13 18:52:59 -0700

    feat: Fetch lists for individual board pages

--- app/(app)/(dashboard)/board/[boardId]/page.tsx
+++ app/(app)/(dashboard)/board/[boardId]/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
+import { database } from '@/lib/database';
 
 interface BoardIdPageProps {
   params: {
     boardId: string;
   };
 };
+
 export default async function BoardIdPage({
   params
 }: BoardIdPageProps) {
@@ -15,6 +17,29 @@ export default async function BoardIdPage({
   if (!orgId) {
     return redirect('/org-selection');
   }
+
+  // Retrieve lists for the individual board ID in ascending order.
+  // Additionally, verify that the related board associated with each list
+  // has a matching orgId for the current user.
+  // Include cards within each list, also sorted in ascending order.
+  const lists = await database.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    }
+  });
 
   return (
     <div>
