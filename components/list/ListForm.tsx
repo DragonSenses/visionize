ee809969d47a73db78b4cf5de7a3f8a8b6589f2a
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-15 23:36:26 -0700

    feat: Enable editing mode for ListForm component

--- components/list/ListForm.tsx
+++ components/list/ListForm.tsx
@@ -1,17 +1,37 @@
 "use client";
 
-import React from 'react';
+import React, { ElementRef, useRef, useState } from 'react';
 import { Plus } from 'lucide-react';
 
 import ListWrapper from './ListWrapper';
 
 export default function ListForm() {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  function disableEditing() {
+    setIsEditing(false);
+  }
+
+  /**
+   * Enables editing mode and focus input.
+   */
+  function enableEditing() {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    })
+  }
+
   return (
     <ListWrapper>
       <button
         className='flex items-center w-full rounded-md p-3 font-medium text-sm bg-white/80 hover:bg-white/50 transition'
       >
-        <Plus className='h-4 w-4 mr-2'/>
+        <Plus className='h-4 w-4 mr-2' />
         Add list
       </button>
     </ListWrapper>
