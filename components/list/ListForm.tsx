4d8f4925c582e1504398943d6099655ba73c2565
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-15 22:40:39 -0700

    style: Enhance visual appearance of ListForm

--- components/list/ListForm.tsx
+++ components/list/ListForm.tsx
@@ -8,8 +8,10 @@ import ListWrapper from './ListWrapper';
 export default function ListForm() {
   return (
     <ListWrapper>
-      <button>
-        <Plus />
+      <button
+        className='flex items-center w-full rounded-md p-3 font-medium text-sm bg-white/80 hover:bg-white/50 transition'
+      >
+        <Plus className='h-4 w-4 mr-2'/>
         Add list
       </button>
     </ListWrapper>
