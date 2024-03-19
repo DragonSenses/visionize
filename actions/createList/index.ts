e2e8e0d60efb315579700984feaf6e7ceae194e2
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-18 23:35:09 -0700

    Create index.ts

---
+++ actions/createList/index.ts
@@ -0,0 +1,48 @@
+"use server";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+
+import { createServerAction } from "@/lib/createServerAction";
+import { database } from "@/lib/database";
+
+import { CreateList } from "./createListSchema";
+import { InputType, OutputType } from "./createListTypes";
+
+async function performAction (data: InputType): Promise<OutputType> {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { title, id } = data;
+
+  let board;
+
+  try {
+    board = await database.board.update({
+      where: {
+        id,
+        orgId,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to update board.'
+    }
+  }
+
+  revalidatePath(`/board/${id}`);
+
+  // Return the updated board
+  return {
+    data: board
+  };
+}
+
+export const createList = createServerAction(CreateList, performAction);
