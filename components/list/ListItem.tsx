5e71c556f15f2f7fa737cde1e0e1d13c24b66c4b
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-20 18:57:32 -0700

    feat: Define prop types for ListItem component

--- components/list/ListItem.tsx
+++ components/list/ListItem.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import React from 'react';
 
-export default function ListItem() {
+import { ListWithCards } from '@/types/types';
+
+interface ListItemProps{
+  data: ListWithCards;
+  index: number;
+}
+
+export default function ListItem({
+  data,
+  index,
+}: ListItemProps) {
   return (
     <div>ListItem</div>
   )
