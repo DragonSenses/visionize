645d25a138f5c592af894d089407a86b2d2fcf09
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-20 19:18:01 -0700

    style: Enhance ListHeader layout & style
    - Refine layout with padding, font adjustments, and flexbox alignment.
    
    Enhanced the ListHeader component's visual appeal and user experience with refined padding, font adjustments for improved readability, and flexbox alignment for a cleaner layout.

--- components/list/ListHeader.tsx
+++ components/list/ListHeader.tsx
@@ -12,6 +12,10 @@ export default function ListHeader({
   data,
 }: ListHeaderProps) {
   return (
-    <div>{data.title}</div>
+    <div className='flex pt-2 px-2 text-sm font-semibold justify-between items-start gap-x-2'>
+      <div className='h-7 w-full px-2.5 py-1 text-sm font-medium border-transparent'>
+        {data.title}
+      </div>
+    </div>
   )
 }
