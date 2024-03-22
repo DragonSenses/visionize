21b75078fcf904068c5d21273061db520ea8a935
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-21 19:05:03 -0700

    feat(ListHeader): Enhance UX with clickable title and edit mode toggle
    - Introduce conditional rendering to switch between a form for editing and a static view of the list title.
    - Implement an `onClick` event on the title div that triggers `enableEditing`, allowing users to enter edit mode directly by clicking on the title.
    - The update enhances the user experience by making the title interaction more intuitive and the transition to edit mode seamless.

--- components/list/ListHeader.tsx
+++ components/list/ListHeader.tsx
@@ -33,9 +33,16 @@ export default function ListHeader({
 
   return (
     <div className='flex pt-2 px-2 text-sm font-semibold justify-between items-start gap-x-2'>
-      <div className='h-7 w-full px-2.5 py-1 text-sm font-medium border-transparent'>
-        {data.title}
-      </div>
+      {isEditing ? (
+        <p>Form</p>
+      ) : (
+        <div 
+          onClick={enableEditing}
+          className='h-7 w-full px-2.5 py-1 text-sm font-medium border-transparent'
+        >
+          {title}
+        </div>
+      )}
     </div>
   )
 }
