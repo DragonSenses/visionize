d894a48556d0e54224dd9fff92e92e9b165a7f4d
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-22 22:34:39 -0700

    feat: Implement validation for UpdateList schema with Zod
    feat: Implement validation for UpdateList schema with Zod
    
    - Introduce Zod schema validation to ensure data integrity for UpdateList.
    - Enforce minimum length requirement for 'title' to enhance data quality.

--- actions/updateList/updateListSchema.ts
+++ actions/updateList/updateListSchema.ts
@@ -5,5 +5,12 @@ import { z } from 'zod';
  * 
  */
 export const UpdateList = z.object({
-
+  title: z.string({
+    required_error: "Title is required", 
+    invalid_type_error: "Title is required", 
+  }).min(3, {
+    message: "Must be 3 or more characters long.", 
+  }),
+  id: z.string(),
+  boardId: z.string(),
 });
