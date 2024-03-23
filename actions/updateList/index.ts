a4725cc091bf8d41b158b11e2b151037a40560eb
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-22 23:09:36 -0700

    feat: Implement UpdateList handler for secure list updates
    - Add server-side action handler to process list updates with authorization checks.
    - Utilize 'auth' for user and organization ID verification to ensure secure transactions.
    - Integrate 'revalidatePath' to refresh list paths post-update for immediate UI consistency.

--- actions/updateList/index.ts
+++ actions/updateList/index.ts
@@ -9,7 +9,40 @@ import { UpdateList } from "./updateListSchema";
 import { InputType, OutputType } from "./updateListTypes";
 
 async function performAction (data: InputType): Promise<OutputType> {
+  const { userId, orgId } = auth();
 
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { title, id } = data;
+
+  let list;
+
+  try {
+    list = await database.list.update({
+      where: {
+        id,
+        orgId,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to update list.'
+    }
+  }
+
+  revalidatePath(`/list/${id}`);
+
+  // Return the updated list
+  return {
+    data: list
+  };
 }
 
 export const updateList = createServerAction(UpdateList, performAction);
