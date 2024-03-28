2bb0f5bf5b9cd5f8d88b1e84a55081edb92f331b
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-28 16:54:13 -0700

    feat: Define prop types for CardForm component

--- components/card/CardForm.tsx
+++ components/card/CardForm.tsx
@@ -2,7 +2,19 @@
 
 import React from 'react';
 
-export default function CardForm() {
+interface CardFormProps {
+  listId: string;
+  isEditing: boolean;
+  disableEditing: () => void;
+  enableEditing: () => void;
+}
+
+export default function CardForm({
+  listId,
+  isEditing,
+  disableEditing,
+  enableEditing,
+}: CardFormProps) {
   return (
     <div>CardForm</div>
   )
