8d7e10aa9e98fb48c30073ab5251993c03e0f2b9
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-30 21:17:35 -0700

    feat: Define prop types for FormTextArea component

--- components/form/FormTextArea.tsx
+++ components/form/FormTextArea.tsx
@@ -1,6 +1,22 @@
 "use client";
 
-import React from 'react';
+import React, { KeyboardEventHandler } from 'react';
+
+interface FormTextAreaProps {
+  id: string;
+  label?: string;
+  value?: string;
+  defaultValue?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors: Record<string, string[] | undefined>;
+  className?: string;
+  onBlur?: () => void;
+  onClick?: () => void;
+  onChange?: () => void;
+  onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
+}
 
 export default function FormTextArea() {
   return (
