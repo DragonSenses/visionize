5102e4d30a79b8cb64eacd6296b130eff2985d5a
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-09 23:57:01 -0700

    feat: Integrate drag-and-drop functionality into ListContainer
    In this commit, the ListContainer component is wrapped with the DragDropContext and Droppable components from the @hello-pangea/dnd package. This enables beautiful and accessible drag-and-drop interactions for lists within the application.

--- components/list/ListContainer.tsx
+++ components/list/ListContainer.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React, { useState } from 'react';
+import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 
 import { ListWithCards } from '@/types/types';
+
 import ListForm from '@/components/list/ListForm';
 import ListItem from '@/components/list/ListItem';
 
@@ -18,20 +20,24 @@ export default function ListContainer({
   const [orderedListData, setOrderedListData] = useState(data);
 
   return (
-    <ol className='flex h-full gap-x-3'>
-      {
-        orderedListData.map((list, index) => {
-          return (
-            <ListItem
-              key={list.id}
-              index={index}
-              data={list}
-            />
-          )
-        })
-      }
-      <ListForm />
-      <div className='flex-shrink-0 w-1' />
-    </ol>
+    <DragDropContext>
+      <Droppable>
+      <ol className='flex h-full gap-x-3'>
+        {
+          orderedListData.map((list, index) => {
+            return (
+              <ListItem
+                key={list.id}
+                index={index}
+                data={list}
+              />
+            )
+          })
+        }
+        <ListForm />
+        <div className='flex-shrink-0 w-1' />
+      </ol>
+      </Droppable>
+    </DragDropContext>
   )
 }
