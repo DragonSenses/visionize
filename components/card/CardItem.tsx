99fcb20000cf5254af7c2a4b864c68e09533e78b
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-09 22:52:35 -0700

    feat: Define prop interface for CardItem

--- components/card/CardItem.tsx
+++ components/card/CardItem.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { Card } from '@prisma/client';
 import React from 'react';
 
-export default function CardItem() {
+interface CardItemProps {
+  data: Card,
+  index: number;
+}
+
+export default function CardItem({
+  data,
+  index,
+}: CardItemProps) {
   return (
     <div>CardItem</div>
   )
