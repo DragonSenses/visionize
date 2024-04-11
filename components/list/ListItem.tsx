210791b98f45c2c2fe60d70795e360484342c36a
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-10 23:27:17 -0700

    feat: Implement draggable behavior in ListItem

--- components/list/ListItem.tsx
+++ components/list/ListItem.tsx
@@ -37,35 +37,44 @@ export default function ListItem({
       draggableId={data.id}
       index={index}
     >
-      <li className='h-full w-72 shrink-0 select-none'>
-        <div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
-          <ListHeader
-            data={data}
-            handleAddCardToList={enableEditing}
-          />
-          <ol
-            className={cn(
-              'flex flex-col gap-y-2 mx-1 px-1 py-0.5',
-              data.cards.length > 0 ? 'mt-2' : 'mt-0'
-            )}
+      {(provided) => (
+        <li
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          className='h-full w-72 shrink-0 select-none'
+        >
+          <div 
+            {...provided.dragHandleProps}
+            className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'
           >
-            {data.cards.map((card, index) => (
-              <CardItem
-                key={card.id}
-                data={card}
-                index={index}
-              />
-            ))}
-          </ol>
-          <CardForm
-            ref={textAreaRef}
-            listId={data.id}
-            isEditing={isEditing}
-            enableEditing={enableEditing}
-            disableEditing={disableEditing}
-          />
-        </div>
-      </li>
+            <ListHeader
+              data={data}
+              handleAddCardToList={enableEditing}
+            />
+            <ol
+              className={cn(
+                'flex flex-col gap-y-2 mx-1 px-1 py-0.5',
+                data.cards.length > 0 ? 'mt-2' : 'mt-0'
+              )}
+            >
+              {data.cards.map((card, index) => (
+                <CardItem
+                  key={card.id}
+                  data={card}
+                  index={index}
+                />
+              ))}
+            </ol>
+            <CardForm
+              ref={textAreaRef}
+              listId={data.id}
+              isEditing={isEditing}
+              enableEditing={enableEditing}
+              disableEditing={disableEditing}
+            />
+          </div>
+        </li>
+      )}
     </Draggable>
   )
 }
