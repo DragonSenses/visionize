b04d6ed7031b47de57dd4abfb5b78cc34900c547
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-12 23:57:21 -0700

    Create updateListOrderTypes.ts

---
+++ actions/updateListOrder/updateListOrderTypes.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+// Import List, the expected output type, from Prisma client
+import { List } from '@prisma/client';
+
+// Encapsulate the state of various actions (e.g., fetching data, submitting forms, etc.)
+// Provides a structured way to handle errors and manage data flow
+import { ActionState } from '@/lib/createServerAction';
+
+// Import the UpdateListOrder schema (validation rules)
+import { UpdateListOrder } from './updateListOrderOrderSchema';
+
+// Define the input type based on the schema
+export type InputType = z.infer<typeof UpdateListOrder>;
+
+// Define the output data type (ActionState)
+export type OutputType = ActionState<InputType, List[]>;
