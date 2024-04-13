e2f8100da3716e62369375765318fc6c32519dd7
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-12 22:28:22 -0700

    feat: Add reorder function for list reordering
    This commit adds the `reorder` function to the `ListContainer` component. The function allows elements within an array to be moved from one position to another. It is used for reordering lists of cards on a board.

--- components/list/ListContainer.tsx
+++ components/list/ListContainer.tsx
@@ -13,6 +13,14 @@ interface ListContainerProps {
   data: ListWithCards[];
 }
 
+function reorder<Type>(list: Type[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
 export default function ListContainer({
   boardId,
   data,
