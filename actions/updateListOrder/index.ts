46cb533aa25d55a5360174db1d3175253ff3b07d
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-16 23:45:16 -0700

    feat: Add imports & function definitions

--- actions/updateListOrder/index.ts
+++ actions/updateListOrder/index.ts
@@ -1,6 +1,12 @@
-// Enforce server-side execution context for security and performance
 "use server";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
 
+import { createServerAction } from "@/lib/createServerAction";
+import { database } from "@/lib/database";
+
+import { UpdateListOrder } from "./updateListOrderSchema";
+import { InputType, OutputType } from "./updateListOrderTypes";
 
 async function performAction (data: InputType): Promise<OutputType> {
 
