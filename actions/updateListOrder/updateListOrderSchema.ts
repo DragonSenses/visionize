b8c4679e7d98000e41fcdd79f859df24c6105113
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-16 23:13:00 -0700

    feat: Add UpdateListOrder Zod schema
    - Introduce Zod schema validation to ensure data integrity for UpdateListOrder

--- actions/updateListOrder/updateListOrderSchema.ts
+++ actions/updateListOrder/updateListOrderSchema.ts
@@ -6,4 +6,13 @@ import { z } from 'zod';
  */
 export const UpdateListOrder = z.object({
   boardId: z.string(),
+  items: z.array(
+    z.object({
+      id: z.string(),
+      title: z.string(),
+      order: z.number(),
+      createdAt: z.date(),
+      updatedAt: z.date(),
+    }),
+  ),
 });
