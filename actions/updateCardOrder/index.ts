981522d18e56ad73d38438538dd7e04dea0eafcc
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-19 22:06:15 -0700

    feat: Add user authentication to updateCardOrder

--- actions/updateCardOrder/index.ts
+++ actions/updateCardOrder/index.ts
@@ -10,7 +10,19 @@ import { UpdateCardOrder } from "./updateCardOrderSchema";
 import { InputType, OutputType } from "./updateCardOrderTypes";
 
 async function performAction (data: InputType): Promise<OutputType> {
-  // ...
+  // Authenticate the user and get their organization ID
+  const { userId, orgId } = auth();
+
+  // If authentication fails, return an error
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+  
+  return {
+    // ...
+  };
 }
 
 export const updateCardOrder = createServerAction(UpdateCardOrder, performAction);
