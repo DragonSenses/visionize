616ab78e39f4c671f81156e8e92eb54f06793917
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-26 14:16:07 -0700

    feat: Render CardModal within ModalProvider

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -2,8 +2,12 @@
 
 import React from 'react';
 
+import CardModal from '@/components/modals/CardModal';
+
 export default function ModalProvider() {
   return (
-    <div>ModalProvider</div>
+    <>
+      <CardModal />
+    </>
   )
 }
