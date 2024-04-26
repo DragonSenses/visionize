470044b3f1d25bad3069b2c192781f38aee3444c
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-26 14:11:43 -0700

    feat: Connect CardModal with state slices
    This commit adds the useCardModal hook to retrieve the `id`, `isOpen`, and `onClose` state, and assigns them to the corresponding props of the `Dialog` component within the `CardModal`.

--- components/modals/CardModal/index.tsx
+++ components/modals/CardModal/index.tsx
@@ -10,9 +10,18 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 
+import { useCardModal } from '@/hooks/useCardModal';
+
 export default function CardModal() {
+  const id = useCardModal((state) => state.id);
+  const isOpen = useCardModal((state) => state.isOpen);
+  const onClose = useCardModal((state) => state.onClose);
+
   return (
-    <Dialog>
+    <Dialog
+      open={isOpen}
+      onOpenChange={onClose}
+    >
       <DialogContent>
         CardModal
       </DialogContent>
