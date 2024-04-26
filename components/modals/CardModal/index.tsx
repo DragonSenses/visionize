2006e7cb3807cf1519a0d7511946fcc237a17bcb
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-26 13:02:09 -0700

    feat: Add Dialog component in CardModal

--- components/modals/CardModal/index.tsx
+++ components/modals/CardModal/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 export default function CardModal() {
   return (
-    <div>CardModal</div>
+    <Dialog>
+      <DialogContent>
+        CardModal
+      </DialogContent>
+    </Dialog>
   )
 }
