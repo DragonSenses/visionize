50083661abebec25757d2c892852d04fe46c3643
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-27 21:18:46 -0700

    feat: Add useCardModal hook & onClick to CardItem

--- components/card/CardItem.tsx
+++ components/card/CardItem.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Draggable, Droppable } from '@hello-pangea/dnd';
 
 import { Card } from '@prisma/client';
+import { useCardModal } from '@/hooks/useCardModal';
 
 interface CardItemProps {
   data: Card,
@@ -14,6 +15,8 @@ export default function CardItem({
   data,
   index,
 }: CardItemProps) {
+  const cardModal = useCardModal();
+  
   return (
     <Draggable
       draggableId={data.id}
@@ -25,6 +28,7 @@ export default function CardItem({
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(data.id)}
           className='py-2 px-3 bg-white text-sm shadow-sm rounded-md truncate border-2 border-transparent hover:border-black'
         >
           {data.title}
