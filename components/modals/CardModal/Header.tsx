c3e770fbf935364f705f26cf4e8c1c4cb261852e
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-01 07:06:52 -0700

    style: Add custom styles to Header component
    In this commit, custom styles have been applied div container, Layout icon and the FormInput component within the Header. The styles for the FormInput include adjustments to padding, font size, background, and border.

--- components/modals/CardModal/Header.tsx
+++ components/modals/CardModal/Header.tsx
@@ -16,13 +16,14 @@ export default function Header({
   const [title, setTitle] = useState(data?.title);
 
   return (
-    <div>
-      <Layout />
-      <div>
+    <div className='flex items-start gap-x-3 mb-6 w-full' >
+      <Layout className='h-5 w-5 mt-1 text-neutral-700' />
+      <div className='w-full'>
         <form>
           <FormInput
             id='title'
             defaultValue={title}
+            className='relative px-1 -left-1.5 w-[95%] text-xl text-neutral-700 font-semibold bg-transparent border-transparent focus-visible:bg-white focus-visible:border-input mb-0.5 truncate'
           />
         </form>
       </div>
