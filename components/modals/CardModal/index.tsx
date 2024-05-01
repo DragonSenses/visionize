169a1f7f2fc90f89aaed1c677c08880c725e8edd
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-30 23:41:29 -0700

    feat: Fetch card data with useQuery hook

--- components/modals/CardModal/index.tsx
+++ components/modals/CardModal/index.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+// Import necessary dependencies
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+// Import UI components
 import {
   Dialog,
   DialogContent,
@@ -10,13 +14,28 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 
+// Import the reusable data fetcher
+import { fetcher } from '@/lib/fetcher';
+
+// Import custom union type
+import { CardWithList } from '@/types/types';
+
+// Import custom hook for managing card modal state
 import { useCardModal } from '@/hooks/useCardModal';
 
 export default function CardModal() {
+  // Get the card ID, modal state, and close function from the custom hook
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
+  // Fetch card data using the useQuery hook
+  const {data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
+  // Render the dialog with the card content
   return (
     <Dialog
       open={isOpen}
