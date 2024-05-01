95c6099ba349be24abf5c5404dd13fccc32efcad
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-30 22:29:07 -0700

    feat(api): Add authentication to card GET route

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,3 +1,4 @@
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -5,7 +6,11 @@ export async function GET(
   { params }: { params: { cardId: string } }
 ) {
   try {
-    
+    const { userId, orgId } = auth();
+
+    if (!userId || !orgId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
   } catch (error) {
     return new NextResponse("Internal Error", { status: 500 });
   }
