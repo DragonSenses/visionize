f996f75cf007e89a15535afb7abf9f4a2e8665b7
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-30 23:05:08 -0700

    feat(api): Implement GET functionality for card

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+import { database } from "@/lib/database";
+
 export async function GET(
   req: Request,
   { params }: { params: { cardId: string } }
@@ -11,6 +13,26 @@ export async function GET(
     if (!userId || !orgId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const card = await database.card.findUnique({
+      where: {
+        id: params.cardId,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      include: {
+        list: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(card);
   } catch (error) {
     return new NextResponse("Internal Error", { status: 500 });
   }
