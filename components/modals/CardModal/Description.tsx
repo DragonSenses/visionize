fe7a0bc8d55942b59b326c67828395e542862bde
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-04 22:40:42 -0700

    feat: Add skeleton component to Description
    The `Description` component now includes a skeleton UI for loading state. It displays placeholders for the card description.

--- components/modals/CardModal/Description.tsx
+++ components/modals/CardModal/Description.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 
 import { CardWithList } from '@/types/types';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface DescriptionProps {
   data: CardWithList
@@ -13,5 +14,17 @@ export default function Description({
 }: DescriptionProps) {
   return (
     <div>{data.description}</div>
+  )
+}
+
+Description.Skeleton = function DescriptionSkeleton() {
+  return (
+    <div className='flex items-start gap-x-3 w-full'>
+      <Skeleton className='h-6 w-6 bg-neutral-200'/>
+      <div className='w-full'>
+        <Skeleton className='h-6 w-24 mb-2 bg-neutral-200'/>
+        <Skeleton className='h-[78px] w-full mb-2 bg-neutral-200'/>
+      </div>
+    </div>
   )
 }
