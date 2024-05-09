ae62f3c9239c831c9271f80187e2e126296699d7
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-09 16:11:19 -0700

    feat: Conditionally render card actions
    In the CardModal component, the card actions are conditionally rendered based on whether `cardData` is available. If `cardData` is not present, a skeleton loader is displayed; otherwise, the actual card actions are shown using the `Actions` component.

--- components/modals/CardModal/index.tsx
+++ components/modals/CardModal/index.tsx
@@ -25,6 +25,7 @@ import {
 
 import Header from './Header';
 import Description from './Description';
+import Actions from './Actions';
 
 export default function CardModal() {
   // Get the card ID, modal state, and close function from the custom hook
@@ -51,6 +52,7 @@ export default function CardModal() {
         }
         {/* Responsive grid for card description and actions */}
         <div className='grid grid-cols-1 md:grid-cols-4 md:gap-4'>
+          {/* Card Description */}
           <div className='col-span-3'>
             <div className='w-full space-y-6'>
               {!cardData
@@ -59,6 +61,11 @@ export default function CardModal() {
               }
             </div>
           </div>
+          {/* Card Actions */}
+          {!cardData
+            ? <Actions.Skeleton />
+            : <Actions data={cardData} />
+          }
         </div>
       </DialogContent>
     </Dialog>
