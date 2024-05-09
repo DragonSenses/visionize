f5743e30f7cce4fd8c20f826abac184d16a6b02b
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-08 23:14:15 -0700

    feat: Create card actions skeleton placeholder

--- components/modals/CardModal/Actions.tsx
+++ components/modals/CardModal/Actions.tsx
@@ -2,8 +2,20 @@
 
 import React from 'react';
 
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function Actions() {
   return (
     <div>Actions</div>
+  )
+}
+
+Actions.Skeleton = function ActionsSkeleton() {
+  return (
+    <div className='mt-2 space-y-2'>
+      <Skeleton className='w-20 h-4 bg-neutral-200'/>
+      <Skeleton className='w-full h-8 bg-neutral-200'/>
+      <Skeleton className='w-full h-8 bg-neutral-200'/>
+    </div>
   )
 }
