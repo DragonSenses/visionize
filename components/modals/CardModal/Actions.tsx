29f690c94815427c050cbc87acf279ffe65ea3ad
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-09 18:24:35 -0700

    feat: Add button and icons to CardModal Actions

--- components/modals/CardModal/Actions.tsx
+++ components/modals/CardModal/Actions.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React from 'react';
+import { Copy, Trash } from 'lucide-react';
 
 import { CardWithList } from '@/types/types';
+import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
 
 interface ActionsProps {
@@ -13,16 +15,29 @@ export default function Actions({
   data,
 }: ActionsProps) {
   return (
-    <div>Actions</div>
+    <div className='mt-2 space-y-2'>
+      <p className='text-xs font-semibold'>
+        Actions
+      </p>
+      {/* Buttons */}
+      <Button>
+        <Copy className='h-4 w-4 mr-2' />
+        Copy
+      </Button>
+      <Button>
+        <Trash className='h-4 w-4 mr-2' />
+        Delete
+      </Button>
+    </div>
   )
 }
 
 Actions.Skeleton = function ActionsSkeleton() {
   return (
     <div className='mt-2 space-y-2'>
-      <Skeleton className='w-20 h-4 bg-neutral-200'/>
-      <Skeleton className='w-full h-8 bg-neutral-200'/>
-      <Skeleton className='w-full h-8 bg-neutral-200'/>
+      <Skeleton className='w-20 h-4 bg-neutral-200' />
+      <Skeleton className='w-full h-8 bg-neutral-200' />
+      <Skeleton className='w-full h-8 bg-neutral-200' />
     </div>
   )
 }
