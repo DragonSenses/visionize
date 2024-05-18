7aa577dc015a0c955a73c4ac67aa37adbd5f3dec
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-18 06:10:48 -0700

    feat: Add authentication to audit log creation

--- lib/createAuditLog.ts
+++ lib/createAuditLog.ts
@@ -1,3 +1,4 @@
+import { auth, currentUser } from "@clerk/nextjs";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 interface AuditLogProps {
@@ -9,8 +10,16 @@ interface AuditLogProps {
 
 export async function createAuditLog(props: AuditLogProps) {
   try {
+    const { orgId } = auth();
+    const user = await currentUser();
+
+    if (!user || !orgId) {
+      throw new Error("User not found.");
+    }
+    
     // Create an audit log
-  } catch(error) {
-    console.error(error);
+  } catch (error) {
+    // Handle any error that may occur within the scope of the audit log
+    console.error("[AUDIT_LOG_ERROR]", error);
   }
 }
