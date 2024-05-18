1f8aabfabca6b78d05c30cf37fa2925fad8bb55f
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-18 06:08:23 -0700

    feat: Define prop types for AuditLog on creation

--- lib/createAuditLog.ts
+++ lib/createAuditLog.ts
@@ -1,4 +1,13 @@
-export async function createAuditLog() {
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
+interface AuditLogProps {
+  entityId: StringConstructor;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+}
+
+export async function createAuditLog(props: AuditLogProps) {
   try {
     // Create an audit log
   } catch(error) {
