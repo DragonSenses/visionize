f0fb138551cfc0829ad296b928ae3822216de2f6
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-23 22:15:51 -0700

    feat: Define prop types for Activity component

--- components/modals/CardModal/Activity.tsx
+++ components/modals/CardModal/Activity.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import React from 'react';
+import { AuditLog } from '@prisma/client';
 
 import { Skeleton } from '@/components/ui/skeleton';
 
-export default function Activity() {
+interface ActitvityProps {
+  data: AuditLog[];
+}
+
+export default function Activity({
+  data,
+}: AuditLog) {
   return (
     <div>Activity</div>
   )
@@ -13,10 +20,10 @@ export default function Activity() {
 Activity.Skeleton = function ActivitySkeleton() {
   return (
     <div className='flex items-start gap-x-3 w-full'>
-      <Skeleton className='h-6 w-6 bg-neutral-200'/>
+      <Skeleton className='h-6 w-6 bg-neutral-200' />
       <div className='w-full'>
-        <Skeleton className='w-24 h-6 mb-2 bg-neutral-200'/>
-        <Skeleton className='w-full h-10 bg-neutral-200'/>
+        <Skeleton className='w-24 h-6 mb-2 bg-neutral-200' />
+        <Skeleton className='w-full h-10 bg-neutral-200' />
       </div>
     </div>
   )
