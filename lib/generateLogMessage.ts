455bfa8880e7d328c39eb0b0258b38e1c000b4c8
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-24 22:44:23 -0700

    feat: Add generateLogMessage utility for AuditLogs

--- lib/generateLogMessage.ts
+++ lib/generateLogMessage.ts
@@ -3,5 +3,18 @@ import { ACTION, AuditLog } from "@prisma/client";
 export function generateLogMessage(log: AuditLog) {
   const { action, entityTitle, entityType } = log;
 
-  console.log(log);
+  if (!action || !entityTitle || !entityType) {
+    return `Error: could not generate message for audit log\n ${log}`;
+  }
+
+  switch (action) {
+    case ACTION.CREATE:
+      return `created ${entityType.toLowerCase()} "${entityTitle}"`;
+    case ACTION.DELETE:
+      return `deleted ${entityType.toLowerCase()} "${entityTitle}"`;
+    case ACTION.UPDATE:
+      return `updated ${entityType.toLowerCase()} "${entityTitle}"`;
+    default:
+      return `unknown action performed on ${entityType.toLowerCase()} "${entityTitle}"`;
+  }
 }
