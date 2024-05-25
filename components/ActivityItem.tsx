ba80d871bd2d7bf5e20ba377b7dddeb7d2f58485
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-24 22:47:13 -0700

    feat: Define prop types for ActivityItem component

--- components/ActivityItem.tsx
+++ components/ActivityItem.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { AuditLog } from '@prisma/client';
 
-export default function ActivityItem() {
+interface ActivityItemProps {
+  data: AuditLog;
+}
+
+export default function ActivityItem({
+  data
+}: ActivityItemProps) {
   return (
     <div>ActivityItem</div>
   )
