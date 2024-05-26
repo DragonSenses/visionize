4ddd71746c0762f9abcc19cbd4ccdea9d54eec4f
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-25 22:27:06 -0700

    style(ActivityItem): Add alignment and adjust text

--- components/ActivityItem.tsx
+++ components/ActivityItem.tsx
@@ -13,17 +13,17 @@ export default function ActivityItem({
   data
 }: ActivityItemProps) {
   return (
-    <li>
-      <Avatar>
+    <li className='flex items-center gap-x-2'>
+      <Avatar className='h-8 w-8'>
         <AvatarImage src={data.userImage} />
       </Avatar>
-      <div>
-        <p>
-          <span>
+      <div className='flex flex-col space-y-0.5'>
+        <p className='text-sm text-muted-foreground'>
+          <span className='font-semibold lowercase text-neutral-700'>
             {data.userName}
           </span> {generateLogMessage(data)}
         </p>
-        <p>
+        <p className='text-xs text-muted-foreground'>
           {format(new Date(data.createdAt), "MMM d, yyyy 'at' h:mm a")}
         </p>
       </div>
