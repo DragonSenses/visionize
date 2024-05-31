45338d8aef3dc7ee7307ea1d4472b8ca96c101e4
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-30 22:30:19 -0700

    feat: Add authentication to ActivityList component

--- components/ActivityList.tsx
+++ components/ActivityList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-export default function ActivityList() {
+export default async function ActivityList() {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect('/org-selection');
+  }
+
   return (
     <div>ActivityList</div>
   )
