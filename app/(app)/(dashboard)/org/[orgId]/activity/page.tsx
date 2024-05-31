8cf4025c86faf07611ad1b9b80e96db39de619b4
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-30 22:24:47 -0700

    feat: Add info section & ActivityList component

--- app/(app)/(dashboard)/org/[orgId]/activity/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/activity/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import ActivityList from '@/components/ActivityList';
+import Info from '@/components/Info';
+import { Separator } from '@/components/ui/separator';
+
 export default function ActivityPage() {
   return (
-    <div>ActivityPage</div>
+    <div className='w-full'>
+      <Info />
+      <Separator className='my-2'/>
+      <ActivityList />
+    </div>
   )
 }
