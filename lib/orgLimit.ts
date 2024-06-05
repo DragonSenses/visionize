e4c1545284c860f4f55dec77171784a3891019ab
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-05 15:47:23 -0700

    feat: Increment available board count

--- lib/orgLimit.ts
+++ lib/orgLimit.ts
@@ -6,7 +6,31 @@ import { database } from "@/lib/database";
 /**
  * Increments the available board count.
  */
-export async function incrementAvailableCount() {}
+export async function incrementAvailableBoardCount() {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    throw new Error("Unauthorized");
+  }
+
+  // Fetch the orgLimit
+  const orgLimit = await database.orgLimit.findUnique({
+    where: { orgId },
+  });
+
+  if (orgLimit) {
+    await database.orgLimit.update({
+      where: { orgId },
+      data: { count: orgLimit.count + 1 }
+    });
+  } else {
+    // If orgLimit does not exist, it could indicate that a board is being
+    // create for the first time. So create the orgLimit with a count of 1.
+    await database.orgLimit.create({
+      data: { orgId, count: 1}
+    });
+  }
+}
 
 /**
  * Decrements the available board count.
