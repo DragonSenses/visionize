c0e629d8b40e572490a29017f92bcbaa54417653
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-14 06:51:07 -0700

    feat: Extract reusable BoardCountDisplay component
    - Create a separate BoardCountDisplay component responsible for displaying the remaining board count
    - Move remaining board count calculation logic out of the main component
    - Improve code organization and maintainability

--- components/BoardCountDisplay.tsx
+++ components/BoardCountDisplay.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import { FREE_BOARD_THRESHOLD } from '@/constants/boards';
+
 interface BoardCountDisplayProps {
   remainingBoardCount: number;
 }
 
-export default function BoardCountDisplay({
+export default async function BoardCountDisplay({
   remainingBoardCount,
 }: BoardCountDisplayProps) {
   return (
-    <div>BoardCountDisplay</div>
+    <div>
+      <span className='text-xs'>
+        {`${ FREE_BOARD_THRESHOLD - remainingBoardCount } remaining`}
+      </span>
+    </div>
   )
 }
