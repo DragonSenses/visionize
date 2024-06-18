2f9fb00cfd12606e966b0b2865406d238b4dab24
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-17 21:29:52 -0700

    Create checkSubscription.ts

---
+++ lib/checkSubscription.ts
@@ -0,0 +1,12 @@
+
+
+/**
+ * Checks the subscription
+ * @param {string} orgId - The organization ID.
+ */
+export async function checkSubscription(orgId: string) {
+  if (!orgId) {
+    return false;
+  }
+
+}
