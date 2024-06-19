31139d57a67a99860734bc750a043238a18ba95c
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-18 22:26:37 -0700

    feat: Access upgrade modal state and functions

--- components/modals/UpgradeModal.tsx
+++ components/modals/UpgradeModal.tsx
@@ -2,7 +2,12 @@
 
 import React from 'react';
 
+import { useUpgradeModal } from '@/hooks/useUpgradeModal';
+
 export default function UpgradeModal() {
+  // Access the upgrade modal state and functions
+  const upgradeModal = useUpgradeModal();
+
   return (
     <div>UpgradeModal</div>
   )
