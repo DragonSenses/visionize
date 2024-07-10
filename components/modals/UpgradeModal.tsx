453dc83cd04d965b1cfedae73d7600e4dae5da51
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-09 22:17:57 -0700

    feat: Add dialog and image to UpgradeModal

--- components/modals/UpgradeModal.tsx
+++ components/modals/UpgradeModal.tsx
@@ -1,14 +1,32 @@
 "use client";
 
 import React from 'react';
+import Image from 'next/image';
 
 import { useUpgradeModal } from '@/hooks/useUpgradeModal';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 
 export default function UpgradeModal() {
   // Access the upgrade modal state and functions
   const upgradeModal = useUpgradeModal();
 
   return (
-    <div>UpgradeModal</div>
+    <Dialog
+      open={upgradeModal.isOpen}
+      onOpenChange={upgradeModal.onClose}
+    >
+      <DialogContent
+        className='max-w-md p-0 overflow-hidden'
+      >
+        <div className='relative flex items-center justify-center aspect-video'>
+          <Image
+            src='/hero.jpg'
+            alt='Hero image'
+            className='object-cover'
+            fill
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
