0605ce55fe64f8716e59ed4caf743f4ee92ad378
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-12 22:13:32 -0700

    feat: Capture Stripe-Signature from req headers

--- app/api/webook/route.ts
+++ app/api/webook/route.ts
@@ -1,9 +1,11 @@
 import Stripe from 'stripe';
+import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 import { database } from '@/lib/database';
 import { stripe } from '@/lib/stripe';
 
 export async function POST(req: Request) {
-  
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 }
