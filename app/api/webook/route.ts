557513b034d33c36926bf21d25a504e5b640d95b
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-12 22:12:57 -0700

    feat: Set up Stripe and database integration

--- app/api/webook/route.ts
+++ app/api/webook/route.ts
@@ -1,4 +1,8 @@
+import Stripe from 'stripe';
 import { NextResponse } from 'next/server';
+
+import { database } from '@/lib/database';
+import { stripe } from '@/lib/stripe';
 
 export async function POST(req: Request) {
   
