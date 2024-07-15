3588f2cd7424a4ad83a4a90b42ce44c1bf3649d0
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-14 22:27:21 -0700

    feat: Produce checkout session from webhook event

--- app/api/webook/route.ts
+++ app/api/webook/route.ts
@@ -8,4 +8,22 @@ import { stripe } from '@/lib/stripe';
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+  } catch (error: any) {
+    // On error, log and return the error message
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  }
+
+  // Successfully constructed event
+
+  // Save the event as a Stripe checkout session
+  const session = event.data.object as Stripe.Checkout.Session;
 }
