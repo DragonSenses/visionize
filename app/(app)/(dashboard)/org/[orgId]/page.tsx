24946197fdfd0e41b72679ffff457f4caa473bf8
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-18 06:35:01 -0700

    feat(OrgPage): Render Info with subscription data
    - Retrieved `orgId` from URL parameters using `useParams`
    - Ensured `orgId` is correctly handled as a string
    - Checked subscription status with `checkSubscription` and stored result in `isSubscribed`
    - Passed `isSubscribed` to `Info` component for conditional rendering

--- app/(app)/(dashboard)/org/[orgId]/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/page.tsx
@@ -1,14 +1,22 @@
 import React, { Suspense } from 'react';
+import { useParams } from 'next/navigation';
 
 import BoardList from '@/components/BoardList';
 import Info from '@/components/Info';
 import { Separator } from '@/components/ui/separator';
+import { checkSubscription } from '@/lib/checkSubscription';
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const { orgId } = useParams();
+
+  // Ensure orgId is a string
+  const orgIdString = Array.isArray(orgId) ? orgId[0] : orgId;
+
+  const isSubscribed = await checkSubscription(orgIdString);
 
   return (
     <div className='flex flex-col w-full mb-20'>
-      <Info />
+      <Info isSubscribed={isSubscribed}/>
       <Separator className='my-4' />
       <div className='px-2 md:px-4'>
         <Suspense fallback={<BoardList.Skeleton />}>
