0670504e42b3b204741fb40968cafbf53fcf9e56
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-18 06:35:13 -0700

    feat(Activity): Render Info with subscription data
    - Retrieved `orgId` from URL parameters using `useParams`
    - Ensured `orgId` is correctly handled as a string
    - Checked subscription status with `checkSubscription` and stored result in `isSubscribed`
    - Passed `isSubscribed` to `Info` component for conditional rendering

--- app/(app)/(dashboard)/org/[orgId]/activity/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/activity/page.tsx
@@ -1,13 +1,21 @@
 import React, { Suspense } from 'react';
+import { useParams } from 'next/navigation';
 
 import ActivityList from '@/components/ActivityList';
 import Info from '@/components/Info';
 import { Separator } from '@/components/ui/separator';
+import { checkSubscription } from '@/lib/checkSubscription';
 
-export default function ActivityPage() {
+export default async function ActivityPage() {
+  const { orgId } = useParams();
+
+  // Ensure orgId is a string
+  const orgIdString = Array.isArray(orgId) ? orgId[0] : orgId;
+
+  const isSubscribed = await checkSubscription(orgIdString);
   return (
     <div className='w-full'>
-      <Info />
+      <Info isSubscribed={isSubscribed}/>
       <Separator className='my-2' />
       <Suspense fallback={<ActivityList.Skeleton />}>
         <ActivityList />
