8db49c6b1390868995763d99ccbc676f260d22f0
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-18 07:09:11 -0700

    feat: Add subscription check and Info component
    - Retrieved `orgId` from URL parameters using `useParams`
    - Ensured `orgId` is correctly handled as a string
    - Checked subscription status with `checkSubscription` and stored result in `isSubscribed`
    - Rendered `Info` component with `isSubscribed` prop

--- app/(app)/(dashboard)/org/[orgId]/billing/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/billing/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useParams } from 'next/navigation';
+
+import { checkSubscription } from '@/lib/checkSubscription';
+import Info from '@/components/Info';
 
 export default async function BillingPage() {
+  const { orgId } = useParams();
+
+  // Ensure orgId is a string
+  const orgIdString = Array.isArray(orgId) ? orgId[0] : orgId;
+
+  const isSubscribed = await checkSubscription(orgIdString);
+  
   return (
-    <div>BillingPage</div>
+    <div className='w-full'>
+      <Info isSubscribed={isSubscribed}/>
+    </div>
   )
 }
