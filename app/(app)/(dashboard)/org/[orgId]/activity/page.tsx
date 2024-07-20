f201a5c9e8ba4c0d0626af2a16c6d1955cc57f0b
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-20 05:52:33 -0700

    fix(ActivityPage): Correct orgId extraction logic
    - Updated ActivityPage to extract orgId using auth() from @clerk/nextjs/server.
    - Added logic to ensure orgId is a string.
    - Integrated checkSubscription to verify subscription status based on orgId.

--- app/(app)/(dashboard)/org/[orgId]/activity/page.tsx
+++ app/(app)/(dashboard)/org/[orgId]/activity/page.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { useParams } from 'next/navigation';
+import { auth } from '@clerk/nextjs/server';
 
 import ActivityList from '@/components/ActivityList';
 import Info from '@/components/Info';
@@ -7,12 +7,13 @@ import { Separator } from '@/components/ui/separator';
 import { checkSubscription } from '@/lib/checkSubscription';
 
 export default async function ActivityPage() {
-  const { orgId } = useParams();
+  const { orgId } = auth();
 
   // Ensure orgId is a string
   const orgIdString = Array.isArray(orgId) ? orgId[0] : orgId;
 
   const isSubscribed = await checkSubscription(orgIdString);
+  
   return (
     <div className='w-full'>
       <Info isSubscribed={isSubscribed}/>
